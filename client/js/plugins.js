48690d7141f7c4581941a7587575fdad516dad3e
gc <grumpy.cola@gmail.com>
2011-09-22 03:52:29 -0400

    updates

--- client/js/plugins.js
+++ client/js/plugins.js
@@ -6,4 +6,6 @@ window.log = function(){
 };
 (function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console||{});
 
-
+pad2 = function(number) {
+	return (number < 10 ? '0' : '') + number;
+};
--- client/js/script.js
+++ client/js/script.js
@@ -1,77 +1,12 @@
-(function($){
+/*!
+ * Script.js
+ * http://mobile.rdacorp.com/
+ *
+ * Copyright (c) 2011 eka renardi
+ * Dual licensed under the MIT and GPL licenses.
+ */
 
-	$("#main").css('display', 'none');
-	$('#loading').fadeOut(500);
 
-	var $container = $('#portfolio');
-	$container.imagesLoaded(function(){
-		$container.masonry({
-			itemSelector : '.box',
-			columnWidth: 100,
-			isAnimated: true
-		});
-	});	
-	
-		
-	$().piroBox({
-		my_speed: 400, //animation speed
-		bg_alpha: 0.3, //background opacity
-		slideShow : true, // true == slideshow on, false == slideshow off
-		slideSpeed : 4, //slideshow duration in seconds(3 to 6 Recommended)
-		close_all : '.piro_close,.piro_overlay'// add class .piro_overlay(with comma)if you want overlay click close piroBox
-	});		
-
-	var Overlay = Backbone.View.extend({
-		el: $("#overlay"),
-		events: {
-			"click": "hide"
-		},
-		hide: function() {
-			this.el.fadeOut(100);
-		}, 
-		show: function() {
-			this.el.fadeIn(100);
-		}
-	})
-
-	var BaseDialog = Backbone.View.extend({
-		overlay: null,
-		events: { 
-			"click .close": "close"
-		},
-
-		initialize: function() {
-			this.el = $(this.el);
-			this.el.hide();
-			this.overlay = new Overlay();
-		},
-
-		show: function() {
-
-			this.overlay.show();
-			this.el.css({
-				"left":"50%",
-				"marginTop":-(this.el.outerHeight() / 2)
-			}).fadeIn(300);
-		},
-
-		close: function() {
-			this.el.fadeOut();
-			this.overlay.hide();
-		}
-	});
-
-	var AboutDlg = BaseDialog.extend({
-		el: $("#about-dlg")
-
-	});
-
-	$("#about").click(function(){
-		var dlg = new AboutDlg();
-		dlg.show();
-	});
-
-	$("#main").fadeIn(500);
-	
-
-})(jQuery);
+new App.Routers.Main();
+App.GalleryView = new App.Views.GalleryView();
+Backbone.history.start();            	
