383d0d554e6ea3ab2e4e971431640f5584a71648
gc <grumpy.cola@gmail.com>
2011-10-06 04:35:35 -0400

    first commit

---
+++ client/js/app.js
@@ -0,0 +1,17 @@
+/*!
+ * app.js
+ * http://mobile.rdacorp.com/
+ *
+ * Copyright (c) 2011 eka[dot]renardi[at]rdacorp[dot]com
+ * Dual licensed under the MIT and GPL licenses.
+ */
+
+var App = App || {};
+
+App = {
+	Views: {},
+	Models: {},
+	Routers: {},
+	
+	GalleryView: null
+};
